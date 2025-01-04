cc2ad4cc88e65e1937f8be9c161b668bd8ec26f7
Sheikh Ahmed <sheikhishtiak@gmail.com>
2025-01-03 22:37:06 -0500

    Specified the key and value serializers in RedisConfig

--- src/main/java/com/sheikh/productmanager/config/RedisConfig.java
+++ src/main/java/com/sheikh/productmanager/config/RedisConfig.java
@@ -1,18 +1,24 @@
 package com.sheikh.productmanager.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+@Configuration
 public class RedisConfig {
     @Bean
-    public RedisTemplate redisTemplate(RedisConnectionFactory connectionFactory){
-        RedisTemplate redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
-
     }
 }
+
